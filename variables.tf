b3fc2afb4b7a8ed2e353bce8db6280bf7eace4c4
Jeremiah Norris <jjno91@gmail.com>
2020-05-14 07:23:52 -0500

    add alb and brittle ecs deployment for testing

---
+++ variables.tf
@@ -0,0 +1,64 @@
+variable "private_subnet_ids" {
+  description = "(Required) IDs of the subnets to which the services and database will be deployed"
+}
+
+variable "public_subnet_ids" {
+  description = "(Required) IDs of the subnets to which the load balancer will be deployed"
+}
+
+variable "domain" {
+  description = "(Required) Domain where the site will be hosted. Example: this.company.com"
+}
+
+variable "zone_id" {
+  description = "(Required) https://www.terraform.io/docs/providers/aws/r/route53_record.html#zone_id"
+}
+
+variable "vpc_id" {
+  description = "(Required) https://www.terraform.io/docs/providers/aws/r/security_group.html#vpc_id"
+}
+
+variable "id" {
+  description = "(Optional) Unique identifier for naming resources"
+  default     = "rancher"
+}
+
+variable "short_id" {
+  description = "(Optional) Short identifier for naming resources that have strict length requirements"
+  default     = "ran"
+}
+
+variable "tags" {
+  description = "(Optional) Tags applied to all resources"
+  default     = {}
+}
+
+variable "hub_image" {
+  description = "(Optional) https://hub.docker.com/r/rancher/rancher"
+  default     = "rancher/rancher"
+}
+
+variable "cpu" {
+  description = "(Optional) https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-cpu-memory-error.html"
+  default     = "1024"
+}
+
+variable "memory" {
+  description = "(Optional) https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-cpu-memory-error.html"
+  default     = "2048" # must be in integer format to maintain idempotency
+}
+
+variable "log_retention" {
+  description = "(Optional) Retention period in days for both ALB and container logs"
+  default     = "90"
+}
+
+variable "protection" {
+  description = "(Optional) Protect ALB and application logs from deletion"
+  default     = false
+}
+
+variable "environment" {
+  description = "(Optional) Additional container environment variables"
+  default     = []
+}
